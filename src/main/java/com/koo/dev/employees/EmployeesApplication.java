5f6636410bd35d6759664c0f9b89407f52c9ff3c
knlr <krystian.limiera@gft.com>
2022-06-06 15:52:09 +0200

    feat/basic auth

--- src/main/java/com/koo/dev/employees/EmployeesApplication.java
+++ src/main/java/com/koo/dev/employees/EmployeesApplication.java
@@ -3,10 +3,13 @@ package com.koo.dev.employees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class EmployeesApplication {
 
     public static void main(String[] args) {
